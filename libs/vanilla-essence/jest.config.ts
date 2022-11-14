fdd732c6761ef0cbc085144f4ba0a0fd3b282325
angrotheman <grosz.antonio@hotmail.de>
2022-11-14 21:50:00 +0100

    renamed lib

--- libs/vanilla-essence/jest.config.ts
+++ libs/vanilla-essence/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'vanilla-extract-plus',
+  displayName: 'vanilla-essence',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/vanilla-extract-plus',
+  coverageDirectory: '../../coverage/libs/vanilla-essence',
 };
