76b430934e1a5672b5b053e9564fc7beff4a8723
angrotheman <grosz.antonio@hotmail.de>
2022-11-15 21:05:42 +0100

    feat: original `style` method as vanillaStyle

--- libs/vanilla-essence/src/lib/vanilla-extract-plus.ts
+++ libs/vanilla-essence/src/lib/vanilla-extract-plus.ts
@@ -3,6 +3,7 @@ export { responsiveStyleV2 as responsiveStyle } from './responsiveStyleV2';
 export { createUniqueIdentifier } from './uniqueIdentifier/index';
 
 export {
+  style as vanillaStyle,
   createVar,
   fallbackVar,
   createTheme,
