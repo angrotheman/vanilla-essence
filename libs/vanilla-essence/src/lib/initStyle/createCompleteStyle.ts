a4ef283a00c22d17b02c0f884705a8d06b5591c1
angrotheman <grosz.antonio@hotmail.de>
2022-12-31 18:27:32 +0100

    cleaned up code

--- libs/vanilla-essence/src/lib/initStyle/createCompleteStyle.ts
+++ libs/vanilla-essence/src/lib/initStyle/createCompleteStyle.ts
@@ -1,19 +1,18 @@
-import {
-  CustomComplexStyle,
-  InitStyleConfig,
-  MagicValueMethods,
-} from '../types';
+import { CustomComplexStyle, InitStyleConfig } from '../types';
 import { style as vanillaStyle } from '@vanilla-extract/css';
 import { createComplexStyleRule } from '../createComplexStyleRule';
+import { createMagicValueCSSClasses } from '../utils/createMagicValueCSSClasses';
 
 export const createCompleteStyle = <C extends InitStyleConfig>({
   config,
-  magicValueMethods,
 }: {
   config: C;
-  magicValueMethods: MagicValueMethods<C['magicProps']>;
 }) => {
   type StyleProps = CustomComplexStyle<C>;
+
+  const magicValueMethods = createMagicValueCSSClasses<C['magicProps']>(
+    config['magicProps']
+  );
 
   const completeStyle = (givenStyle: StyleProps) =>
     vanillaStyle(
--- libs/vanilla-essence/src/lib/initStyle/index.ts
+++ libs/vanilla-essence/src/lib/initStyle/index.ts
@@ -1,34 +1,14 @@
 import type { InitStyleConfig } from '../types';
-import { createMagicValueCSSClasses } from '../utils/createMagicValueCSSClasses';
 import { createThemeSprinkles } from '../sprinkles/createThemeSprinkles';
 import { createCompleteStyle } from './createCompleteStyle';
-// import { createCompleteResponsiveStyle } from './createCompleteResponsiveStyle';
 
 export const initStyle = <C extends InitStyleConfig>(config: C) => {
-  // type ResponsiveStyleProps = CustomResponsiveStyle<C>;
-
-  // ------
-
-  const magicValueMethods = createMagicValueCSSClasses<C['magicProps']>(
-    config['magicProps']
-  );
-
-  // ------
-
-  const completeStyle = createCompleteStyle({ config, magicValueMethods });
-
-  /*
-  const completeResponsiveStyle = createCompleteResponsiveStyle({
-    config,
-    magicValueMethods,
-  });
-  */
+  const completeStyle = createCompleteStyle({ config });
 
   const sprinkles = createThemeSprinkles({ config });
 
   return {
     style: completeStyle,
-    // responsiveStyle: completeResponsiveStyle,
     themeSprinkles: sprinkles,
   };
 };
